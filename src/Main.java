51204434f063e32a98628f76569531c2f74b42b4
anzegre <kapitansasa2@gmail.com>
2024-06-09 21:30:01 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] numbers = new double[15]
+        System.out.println(numbers[3]);
+        numbers[0] = 1.5;
+        numbers[1] = 2.3;
+        numbers[2] = -1.2;
+        numbers[3] = 4.7;
+        numbers[4] = -3.5;
+        numbers[5] = 5.5;
+        numbers[6] = -0.4;
+        numbers[7] = 3.3;
+        numbers[8] = -2.1;
+        numbers[9] = 1.8;
+        numbers[10] = -0.9;
+        numbers[11] = 2.7;
+        numbers[12] = -1.4;
+        numbers[13] = 3.9;
+        numbers[14] = -2.8;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers[" + i + "] = " + numbers[i]);
+
+
+    }
+}
